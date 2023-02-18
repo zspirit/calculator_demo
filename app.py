27a4a1024a28dd0331826fa0cf4ee15cc4d96eab
zspirit <zgafaoui@gmail.com>
2023-02-18 10:04:15 +0100

    Update app.py
    code change / refactoring

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+from flask_restx import Resource, Api, reqparse
+from apispec import APISpec
+from marshmallow import Schema, fields
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
+from flask_apispec.views import MethodResource
+from flask_apispec import marshal_with, doc, use_kwargs
 
-app = Flask(__name__)
-app.config["DEBUG"]=True
+app = Flask(__name__)  # Flask app instance initiated
+api = Api(app,
+          version='1.0',
+          title='Simple Calculator API',
+          description='This is a simple demo calculator API ')  # Flask restful wraps Flask app around it.
+ns = api.namespace('/',
+                   description='Demo : Simple Calculator')
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Calculator Demo',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+})
+docs = FlaskApiSpec(app)
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-   try:
-      if "opt" in request.args and "v1" in request.args and "v2" in request.args:
-         result = eval(request.args["v1"]+request.args["opt"]+request.args["v2"] )
-         response = {"operation": request.args, "result": result}
-         return jsonify({'status': 'succes',
-                         'code': 200,
-                         'message': response})
-      else:
-         return jsonify({'status': 'Error',
-                         'code' : 400,
-                         'message': 'Calculation Error : Invalid operation parameters'})
-   except Exception as e:
-      return jsonify({'status': 'Error',
-                         'code' : 400,
-                         'message': str(e)})
+#  Restful way of creating APIs through Flask Restful
+@ns.route('/<string:formula>')
+class CalculatorAPI(MethodResource, Resource):
+    @doc(description='Demo Calculator API.', tags=['GET', 'Calculator', 'API'])
+    def get(self, formula,):
+        try:
+            if '÷' or '/' or '%2F' in formula:
+                formula = formula.replace('÷', '/')
+            return {'status': 'Success',
+                    'code': 200,
+                    'formula': formula,
+                    'result': eval(formula)}
+        except Exception as e:
+            return jsonify({'status': 'Error',
+                            'code': 400,
+                            'formula': formula,
+                            'message': str(e)})
 
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
